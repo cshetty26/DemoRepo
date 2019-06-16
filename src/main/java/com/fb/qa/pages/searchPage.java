39f0d65c7ae32a2a4b83e1324dcd34834fa96bcf
cshetty26 <chaithrashetty1126@gmail.com>
2019-06-16 00:19:35 -0500

    First commit for my selenium code

--- src/main/java/com/fb/qa/pages/searchPage.java
+++ src/main/java/com/fb/qa/pages/searchPage.java
@@ -1,5 +1,14 @@
 package com.fb.qa.pages;
 
 public class searchPage {
+	public void searchpage() {
+		System.out.println("Search page");
+		int a=0;
+		int b = 0;
+		int c = a+b;
+		System.out.println(c);
+		
+	}
 
+	
 }
